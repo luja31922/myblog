f205af9c02294f038cd53384cde11bc720ff9e58
Octavio Lujan <kontara.dev@gmail.com>
2019-07-12 16:02:13 -0500

    version para guardar

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_10_215709) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "city_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-admin = User.find_or_create_by(firt_name:'Andres',second_name:"Lujan" ,email:"luja31922@gmail.com",password: "Cc1053805027",password_confirmation: "Cc1053805027", is_admin: "true")
+admin = User.create!(firt_name:'Andres',second_name:"Lujan" ,email:"luja31922@gmail.com",password: "Cc1053805027",password_confirmation: "Cc1053805027", is_admin: "true")
+puts 'admin'
